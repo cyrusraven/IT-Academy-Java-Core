ed69cc9248605edc5efd3d0599aad558d9f0172a
Nadzeya Petrova <nightmare.evans13@gmail.com>
2023-10-05 23:02:17 +0300

    Changes

--- homework7/src/Task5.java
+++ homework7/src/Task5.java
@@ -1,18 +1,18 @@
 public class Task5 {
     public static void main(String[] args) {
-        String str = " Dog,  dogs, dog, DOG,   doG, cat ";
-        String str1 = str.trim();
+        String originalText = " Dog,  dogs, dog, DOG,   doG, cat ";
+        String normalizedText = originalText.trim();
         String regEx = ",+\\s+";
-        String str2 = str1.replaceAll(regEx, " ");
-        String[] str3 = str2.split(" ");
+        String str2 = normalizedText.replaceAll(regEx, " ");
+        String[] words = str2.split(" ");
 
-        System.out.println(count(str3));
+        System.out.println(count(words));
     }
 
-    public static int count(String[] str3) {
-        int count = 0;
-        for (String string : str3) {
-            String tmp = str3[0];
+    public static int count(String[] words) {
+        int count = 1;
+        for (String string : words) {
+            String tmp = words[0];
             if (!tmp.equalsIgnoreCase(string)) {
                 count++;
             }
