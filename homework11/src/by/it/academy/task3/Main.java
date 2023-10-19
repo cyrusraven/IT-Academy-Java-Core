b411b577f0eaeb495762147125eca8e3028a6f36
Nadzeya Petrova <nightmare.evans13@gmail.com>
2023-10-19 11:28:02 +0300

    Changes

--- homework11/src/by/it/academy/task3/Main.java
+++ homework11/src/by/it/academy/task3/Main.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         NumberGuessing numberGuessing = new NumberGuessing();
+        int attempt  = 100;
 
         while (true) {
             int hiddenNumber = new Random().nextInt(1001);
@@ -15,7 +16,13 @@ public class Main {
             } catch (LessException | GreaterException e) {
                 System.out.println(e.getMessage());
             }
+
+            if (numberGuessing.getAttempt() >= attempt) {
+                System.out.println("Too many tries.");
+                break;
+            }
         }
-        System.out.println("Hidden number: " + numberGuessing.getHiddenNumber());
+        System.out.println("Hidden number: " + numberGuessing.getHiddenNumber() + "\n"
+                + "Attempts: " + numberGuessing.getAttempt());
     }
 }
--- homework11/src/by/it/academy/task3/NumberGuessing.java
+++ homework11/src/by/it/academy/task3/NumberGuessing.java
@@ -4,10 +4,16 @@ import java.util.Random;
 
 public class NumberGuessing {
     private final int hiddenNumber;
+    private int attempt;
 
     public NumberGuessing() {
         Random rand = new Random();
         hiddenNumber = rand.nextInt(1001);
+        attempt = 0;
+    }
+
+    public int getAttempt() {
+        return attempt;
     }
 
     public int getHiddenNumber() {
@@ -15,6 +21,7 @@ public class NumberGuessing {
     }
 
     public void numberGuessingAlgorithm(int number) throws LessException, GreaterException {
+        attempt++;
         if (number < hiddenNumber) {
             throw new LessException("Less than the hidden number.");
         } else if (number > hiddenNumber) {
